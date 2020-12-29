0d021e5d1decafa583b8a2269cf644790ffebeb4
nonameqm <48423723+nonameqm@users.noreply.github.com>
2020-12-30 00:47:32 +0900

    Node_JS [Day1]
    url, file read system

---
+++ web1/main.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url'); //url 모듈 
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title=queryData.id;
+    console.log(queryData.id);
+    if(_url == '/'){
+      title='Welcome';
+    }
+    if(_url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    fs.readFile(`../data/${queryData.id}`, 'utf-8', function(err, data){
+        var description=data;
+        console.log('check : '+description);
+        var template=`
+            <!doctype html>
+            <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+            </body>
+            </html>
+        `
+        response.end(template);
+    })
+
+ 
+});
+app.listen(3000);
