c850fdc7ccbec79b904301592163bd2b05d727de
nonamePaul01 <75108988+nonamePaul01@users.noreply.github.com>
2021-01-04 22:40:20 +0900

    Node_JS [Day2]
    apply js concepts to web application (function)

--- web1/main.js
+++ web1/main.js
@@ -2,42 +2,72 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url'); //url 모듈 
 
+function templateHTML(title, list, body){
+  return `
+    <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+    </body>
+    </html>
+  `;
+}
+
+function templateList(filelist){
+  var list='<ul>';
+  var i=0;
+  while(i<filelist.length){
+    list=list+ `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i=i+1;
+  }
+  list=list+'</ul>'
+  return list;
+}
+
+
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title=queryData.id;
-    console.log(queryData.id);
-    if(_url == '/'){
-      title='Welcome';
+    var pathname = url.parse(_url, true).pathname;
+
+   
+    if(pathname === '/'){
+      if(queryData.id===undefined){
+        fs.readdir('./data', function(errpr, filelist){
+          var title='Welcome';
+          var description='Hello, Node.js';
+          var list=templateList(filelist);
+          var template=templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          response.writeHead(200); //웹서버가 브라우저에게 200이라는 숫자 전송 시 성공
+          response.end(template);
+        })          
+      }
+      else{
+        fs.readdir('./data', function(errpr, filelist){
+          var title='Welcome';
+          var description='Hello, Node.js';
+          var list='<ul>';
+          var list=templateList(filelist);
+          fs.readFile(`./data/${queryData.id}`, 'utf-8', function(err, description){
+            var title=queryData.id;
+            var template=templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            response.writeHead(200); //웹서버가 브라우저에게 200이라는 숫자 전송 시 성공
+            response.end(template);
+          });
+       });
+      }
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
+    else{
+      response.writeHead(404); // 404라는 숫자 전송 시 실패임
+      response.end('Not Found'); 
     }
-    response.writeHead(200);
-    fs.readFile(`../data/${queryData.id}`, 'utf-8', function(err, data){
-        var description=data;
-        console.log('check : '+description);
-        var template=`
-            <!doctype html>
-            <html>
-            <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-            </head>
-            <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-            </body>
-            </html>
-        `
-        response.end(template);
-    })
 
  
 });
