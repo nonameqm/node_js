d6c435513b2fcee327a918dce6c07a77f8c50154
nonameqm <48423723+nonameqm@users.noreply.github.com>
2021-01-17 00:31:02 +0900

    nodejs [day4]

---
+++ nodejs/module.js
@@ -0,0 +1,10 @@
+// var M={
+//     v: 'v',
+//     f: function(){
+//         console.log(this.v)
+//     }
+// }
+// M.f()
+
+var M = require('./modulepart.js')
+M.f()
--- web1/main.js
+++ web1/main.js
@@ -1,35 +1,40 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url"); //url 모듈
+var qs = require('querystring');
 
-function templateHTML(title, list, body) {
-  return `
-    <!doctype html>
-    <html>
-    <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-    </head>
-    <body>
-    <h1><a href="/">WEB1</a></h1>
-    ${list}
-    <a href="/create">create</a>
-    ${body}
-    </body>
-    </html>
-  `;
-}
+var template={
+  html: function(title, list, body, control) {
+    return `
+      <!doctype html>
+      <html>
+      <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+      </head>
+      <body>
+      <h1><a href="/">WEB1</a></h1>
+      ${list}
+      ${control}
+      ${body}
+      </body>
+      </html>
+    `;
+  },
 
-function templateList(filelist) {
-  var list = "<ul>";
-  var i = 0;
-  while (i < filelist.length) {
-    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i = i + 1;
+  list: function(filelist) {
+    var list = "<ul>";
+    var i = 0;
+    while (i < filelist.length) {
+      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      i = i + 1;
+    }
+    list = list + "</ul>";
+    return list;
   }
-  list = list + "</ul>";
-  return list;
 }
+
+
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -37,36 +42,42 @@ var app = http.createServer(function (request, response) {
   var pathname = url.parse(_url, true).pathname;
   if (pathname === "/") {
     if (queryData.id === undefined) {
-      fs.readdir("../data", function (errpr, filelist) {
-        var title = "Welcome";
-        var description = "Hello, Node.js";
-        var list = templateList(filelist);
-        var template = templateHTML(
-          title,
-          list,
-          `<h2>${title}</h2>${description}`
-        );
-        response.writeHead(200); //웹서버가 브라우저에게 200이라는 숫자 전송 시 성공
-        response.end(template);
-      });
-    } else {
       fs.readdir("./data", function (errpr, filelist) {
         var title = "Welcome";
         var description = "Hello, Node.js";
+        var list = template.list(filelist);
+        var html = template.html(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a> `
+        );
+        response.writeHead(200); //웹서버가 브라우저에게 200이라는 숫자 전송 시 성공
+        response.end(html);
+      });
+    } else {
+      fs.readdir("./data", function (errpr, filelist) {
         var list = "<ul>";
-        var list = templateList(filelist);
+        var list = template.list(filelist);
         fs.readFile(
           `./data/${queryData.id}`,
           "utf-8",
           function (err, description) {
             var title = queryData.id;
-            var template = templateHTML(
+            var html = template.html(
               title,
               list,
-              `<h2>${title}</h2>${description}`
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a>
+               <a href="/update?id=${title}">update</a>
+               <form action="delete_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+               </form>
+               `
             );
             response.writeHead(200); //웹서버가 브라우저에게 200이라는 숫자 전송 시 성공
-            response.end(template);
+            response.end(html);
           }
         );
       });
@@ -74,12 +85,12 @@ var app = http.createServer(function (request, response) {
   } else if (pathname === "/create") {
     fs.readdir("./data", function (errpr, filelist) {
       var title = "Web - create";
-      var list = templateList(filelist);
-      var template = templateHTML(
+      var list = template.list(filelist);
+      var html = template.html(
         title,
         list,
         `
-        <form action="http://localhost:3000/process_create" method="post">
+        <form action="http://localhost:3000/create_process" method="post">
           <!-- action string의 서버로 해당 정보를 보냄 -->
           <p><input type="text" name="title" placeholder="title" /></p>
           <p>
@@ -89,11 +100,89 @@ var app = http.createServer(function (request, response) {
             <input type="submit" />
           </p>
         </form>
-        `
+        `,
+        ''
       );
       response.writeHead(200); //웹서버가 브라우저에게 200이라는 숫자 전송 시 성공
-      response.end(template);
+      response.end(html);
     });
+  } else if(pathname=='/create_process'){
+      var body='';
+      //data를 나눠서 수신함
+      request.on('data', function(data){
+        body=body+data;
+      });
+      //data 수신이 끝났을 때 
+      request.on('end', function(){
+        var post=qs.parse(body);
+        var title=post.title;
+        var description=post.description;
+        fs.writeFile(`data/${title}`, description, 'utf-8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end();
+        });
+      });
+      
+  } else if(pathname === '/update'){
+    fs.readdir("./data", function (errpr, filelist) {
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+        var title = queryData.id;
+        var list = template.list(filelist);
+        var html = template.html(
+          title,
+          list,
+          `
+          <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <!-- action string의 서버로 해당 정보를 보냄 -->
+            <p><input type="text" name="title" placeholder="title" value="${title}" /></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit" />
+            </p>
+          </form>
+          `,
+          `<a href="/create">create</a> `
+        );
+        response.writeHead(200); //웹서버가 브라우저에게 200이라는 숫자 전송 시 성공
+        response.end(html);
+      })
+    });    
+  } else if(pathname === '/update_process'){
+    var body='';
+    //data를 나눠서 수신함
+    request.on('data', function(data){
+      body=body+data;
+    });
+    //data 수신이 끝났을 때 
+    request.on('end', function(){
+      var post=qs.parse(body);
+      var title=post.title;
+      var id=post.id;
+      var description=post.description;
+      fs.writeFile(`data/${title}`, description, 'utf-8', function(err){
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+      });
+    })
+  } else if(pathname === '/delete_process'){
+    var body='';
+    //data를 나눠서 수신함
+    request.on('data', function(data){
+      body=body+data;
+    });
+    //data 수신이 끝났을 때 
+    request.on('end', function(){
+      var post=qs.parse(body);
+      var id=post.id;
+      console.log(id);
+      fs.unlink(`data/${id}`, function(error){
+        response.writeHead(302, {Location: `/`});
+        response.end();
+      });
+    })
   } else {
     response.writeHead(404); // 404라는 숫자 전송 시 실패임
     response.end("Not Found");
